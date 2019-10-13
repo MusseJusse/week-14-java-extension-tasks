462cca24a8c1ada3bd00656daafb1fdc44068997
MusseJusse <54991745+MusseJusse@users.noreply.github.com>
2019-10-13 20:48:20 +0100

    Added comments

--- src/ChallengesTest.java
+++ src/ChallengesTest.java
@@ -1,8 +1,11 @@
 public class ChallengesTest {
 	public static void main(String[] args) {
 		Challenges challenges = new Challenges();
+		//TASK 1
 		System.out.println(challenges.reverseNum(1234));
+		//TASK 2
 		System.out.println(challenges.generatePhone());
+		//TASK 3
 		System.out.println(challenges.madLibs());
 	}
 }
